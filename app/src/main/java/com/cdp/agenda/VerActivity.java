5c143ad637c42575360660ccd1092c0f22f89302
Marko Robles <markorobles75@gmail.com>
2021-03-22 07:59:43 -0600

    Se agrega vista para ver contacto individual y editar Se agrega clase para ver y editar registro Se agrega eventos para seleccionar registros de lista Se agrega método UPDATE a la tabla Contactos

---
+++ app/src/main/java/com/cdp/agenda/VerActivity.java
@@ -0,0 +1,70 @@
+package com.cdp.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.cdp.agenda.db.DbContactos;
+import com.cdp.agenda.entidades.Contactos;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class VerActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtTelefono, txtCorreo;
+    Button btnGuarda;
+    FloatingActionButton fabEditar;
+
+    Contactos contacto;
+    int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreo = findViewById(R.id.txtCorreoElectronico);
+        btnGuarda = findViewById(R.id.btnGuarda);
+        fabEditar = findViewById(R.id.fabEditar);
+
+        if(savedInstanceState == null){
+            Bundle extras = getIntent().getExtras();
+            if(extras == null){
+                id = Integer.parseInt(null);
+            } else {
+                id = extras.getInt("ID");
+            }
+        } else {
+            id = (int) savedInstanceState.getSerializable("ID");
+        }
+
+        DbContactos dbContactos = new DbContactos(VerActivity.this);
+        contacto = dbContactos.verContacto(id);
+
+        if(contacto != null){
+            txtNombre.setText(contacto.getNombre());
+            txtTelefono.setText(contacto.getTelefono());
+            txtCorreo.setText(contacto.getCorreo_electornico());
+            btnGuarda.setVisibility(View.INVISIBLE);
+            txtNombre.setInputType(InputType.TYPE_NULL);
+            txtTelefono.setInputType(InputType.TYPE_NULL);
+            txtCorreo.setInputType(InputType.TYPE_NULL);
+        }
+
+        fabEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VerActivity.this, EditarActivity.class);
+                intent.putExtra("ID", id);
+                startActivity(intent);
+            }
+        });
+    }
+}
